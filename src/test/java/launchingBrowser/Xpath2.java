7b37df29ddf0c28850d77aa3dba424e79b427c6c
Roshni <roshnikc12212@gmail.com>
2026-07-06 21:04:46 +0530

    class updated

--- src/test/java/launchingBrowser/Xpath2.java
+++ src/test/java/launchingBrowser/Xpath2.java
@@ -9,33 +9,35 @@ import org.openqa.selenium.support.locators.RelativeLocator;
 public class Xpath2 {
 public static void main(String[] args) throws InterruptedException {
 	WebDriver driver=new ChromeDriver();
-//	driver.get("https://demowebshop.tricentis.com/");
-//	driver.manage().window().maximize();
-//	driver.findElement(By.xpath("//input[@value='Search']/preceding-sibling::input")).sendKeys("roshni");
-//	
-//	Thread.sleep(2000);
-//	
-//    WebElement ele= driver.findElement(By.linkText("Register"));
-//	driver.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(ele)).click();
-//	
-//	
-//	driver.get("https://demo.vtiger.com/vtigercrm/");
-//	WebElement un= driver.findElement(By.id("username"));
-//	WebElement pw= driver.findElement(By.id("password"));
-//	un.clear();
-//	pw.clear();
-//	Thread.sleep(2000);
-//	un.sendKeys("Roshni");
-//	Thread.sleep(2000);
-//	pw.sendKeys("123");
-//	Thread.sleep(2000);
-//	driver.findElement(By.className("buttonBlue")).click();
-//	
-//	System.out.println(driver.findElement(By.id("validationMessage")).getText());
-//	
-//	
+	driver.get("https://demowebshop.tricentis.com/");
+	driver.manage().window().maximize();
+	driver.findElement(By.xpath("//input[@value='Search']/preceding-sibling::input")).sendKeys("roshni");
+	
+	Thread.sleep(2000);
+	
+    WebElement ele= driver.findElement(By.linkText("Register"));
+	driver.findElement(RelativeLocator.with(By.tagName("a")).toRightOf(ele)).click();
+	
+	
+	driver.get("https://demo.vtiger.com/vtigercrm/");
+	WebElement un= driver.findElement(By.id("username"));
+	WebElement pw= driver.findElement(By.id("password"));
+	un.clear();
+	pw.clear();
+	Thread.sleep(2000);
+	un.sendKeys("Roshni");
+	Thread.sleep(2000);
+	pw.sendKeys("123");
+	Thread.sleep(2000);
+	driver.findElement(By.className("buttonBlue")).click();
+	
+	System.out.println(driver.findElement(By.id("validationMessage")).getText());
+	
+	
 	driver.get("https://www.actitime.com/");
 	driver.manage().window().maximize();
 	System.out.println(driver.findElement(By.linkText("Try actiTIME for Free")).getTagName());
+	
+	driver.close();
 }                      
 }
