d3fab9101257bb4a15c763e78c2f5e2f9382da36
Roshni <roshnikc12212@gmail.com>
2026-07-08 17:58:46 +0530

    login button xpath correction

--- src/test/java/assignment1/Zepto.java
+++ src/test/java/assignment1/Zepto.java
@@ -10,26 +10,31 @@ public class Zepto {
 public static void main(String[] args) throws InterruptedException {
 	WebDriver driver = new ChromeDriver();
 	driver.get("https://www.zepto.com/");
-	Thread.sleep(2000);
+	
 	driver.manage().window().maximize();
 	
 	driver.findElement(By.xpath("//span[text()=\"Search for\"]")).click();
-	Thread.sleep(2000);
+	Thread.sleep(4000);
 	WebElement search= driver.findElement(By.xpath("//input[@placeholder=\"Search for over 5000 products\"]"));
+	Thread.sleep(2000);
 	search.sendKeys("kurkure",Keys.ENTER);
 	Thread.sleep(2000);
 	driver.findElement(By.xpath("//span[text()=\"₹4\"]/../../..//button[text()=\"ADD\"]")).click();
 	Thread.sleep(1000);
 	
+	search.sendKeys(Keys.chord(Keys.CONTROL+"a"));
+	search.sendKeys(Keys.DELETE);
 	
-	search.clear();
+	
 	search.sendKeys("chips",Keys.ENTER);
 	driver.findElement(By.xpath("//span[text()=\"₹3\"]/../../..//button[text()=\"ADD\"]")).click();
 	Thread.sleep(2000);
-	search.clear();
+	search.sendKeys(Keys.chord(Keys.CONTROL+"a"));
+	search.sendKeys(Keys.DELETE);
+	
 	search.sendKeys("doritos",Keys.ENTER);
-	driver.findElement(By.xpath("//span[text()=\"₹12\"]/../../..//button[text()=\"ADD\"]")).click();
+	driver.findElement(By.xpath("//span[text()=\"12₹\"]/../../..//button[text()=\"ADD\"]")).click();
 	driver.close();
-//	
+	
 }
 }
