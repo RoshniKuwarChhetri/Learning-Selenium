2f2fce1e3b9c7a7065753eaf82f6008cb4aa4dcd
Roshni <roshnikc12212@gmail.com>
2026-07-23 21:06:40 +0530

    practice

---
+++ src/test/java/practice1/ChromeBrowser.java
@@ -0,0 +1,12 @@
+package practice1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class ChromeBrowser {
+public static void main(String[] args) {
+	WebDriver driver =new EdgeDriver();
+	
+}
+}
