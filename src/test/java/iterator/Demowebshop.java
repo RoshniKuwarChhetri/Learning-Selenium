bbe0224929d429a366b5dcb126fc45537425affd
RoshniKuwarChhetri <roshnikc12212@gmail.com>
2026-07-31 16:56:27 +0530

    iterating link

---
+++ src/test/java/iterator/Demowebshop.java
@@ -0,0 +1,30 @@
+package iterator;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demowebshop {
+
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demowebshop.tricentis.com/");
+
+        List<WebElement> links = driver.findElements(By.tagName("li"));
+
+        Iterator<WebElement> itr = links.iterator();
+
+        while (itr.hasNext()) {
+            WebElement element = itr.next();
+            System.out.println(element.getText());
+        }
+
+        driver.quit();
+    }
+}
