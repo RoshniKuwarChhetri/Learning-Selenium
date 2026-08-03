a70277986a50051edacec55d7227cd743b03f52f
RoshniKuwarChhetri <roshnikc12212@gmail.com>
2026-08-03 14:58:08 +0530

    third in drop down

--- src/test/java/operations/AmazonAppleProduct.java
+++ src/test/java/operations/AmazonAppleProduct.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Amazon {
+public class AmazonAppleProduct {
 public static void main(String[] args) throws InterruptedException {
 	WebDriver driver = new ChromeDriver();
     driver.get("https://www.amazon.in/");
