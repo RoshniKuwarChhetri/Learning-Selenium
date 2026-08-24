d97071963702876b812bca3174009f96d2d73160
RoshniKuwarChhetri <roshnikc12212@gmail.com>
2026-08-24 19:16:41 +0530

    practice

--- src/test/java/mockPrep/Task2.java
+++ src/test/java/mockPrep/Task2.java
@@ -1,16 +1,27 @@
 package mockPrep;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class Task2 {
 public static void main(String[] args) throws InterruptedException {
 	WebDriver driver = new ChromeDriver();
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
 	driver.manage().window().maximize();
-	driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-	Thread.sleep(3000);
-	driver.findElement(By.xpath("//input[@placeholder=\"Username\"]")).sendKeys("Admin");
-	driver.findElement(By.xpath("//input[@placeholder=\"password\"]")).sendKeys("admin123");
+	driver.get("https://the-internet.herokuapp.com/?utm_source=chatgpt.com");
+    driver.findElement(By.linkText("Dynamic Controls")).click();	
+    driver.findElement(By.xpath("//button[text()=\"Remove\"]")).click();
+    
+    wait.until(ExpectedConditions.invisibilityOf(driver.findElement(By.id("checkbox"))));
+    driver.findElement(By.xpath("//button[text()=\"Add\"]")).click();
+    
+    wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.id("checkbox"))));
+        
 }
 }
