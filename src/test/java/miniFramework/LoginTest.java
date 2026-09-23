47b9c8be0fc6e62dff852d9d148a23088223f0cc
RoshniKuwarChhetri <roshnikc12212@gmail.com>
2026-09-23 23:33:22 +0530

    testNg practice

---
+++ src/test/java/miniFramework/LoginTest.java
@@ -0,0 +1,17 @@
+package miniFramework;
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+public class LoginTest extends BaseTest {
+
+	@Test
+	public void ValidLoginTest() {
+		driver.findElement(By.id("import org.testng.annotations.Test;")).sendKeys("Roshni");
+		driver.findElement(By.id("password")).sendKeys("12345678910");
+		driver.findElement(By.id("login-button")).click();
+		
+		Assert.assertTrue(driver.getCurrentrl().contains())
+	}
+	
+}
+//
